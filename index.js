77663d75d8146558105e13ec22555f8bf2a7208c
sinkoo <69867339+sinkoo0@users.noreply.github.com>
2022-06-07 13:37:30 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// create variable
+
+const restaurant = {
+	name: "IndoLunch",
+  	city: "Jakarta",
+  	"favorite food" : "Nasi Goreng",
+  	"favorite drink" : "Green Tea",
+  	isVegan: true
+}
+let name = "IndoLunch"
+let favoriteDrink = "Green Tea"
+
+console.log(restaurant);
+console.log(name);
+console.log(favoriteDrink);
